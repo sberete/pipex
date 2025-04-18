a88a0dfb233e3662e448d4bab626acca6ad99427
Salim Berete <sberete@bess-f2r4s16.clusters.42paris.fr>
2025-04-18 21:21:47 +0200

    retest

---
+++ includes/pipex_bonus.h
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex_bonus.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sberete <sberete@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/17 21:40:18 by sberete           #+#    #+#             */
+/*   Updated: 2025/04/17 23:04:12 by sberete          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_BONUS_H
+# define PIPEX_BONUS_H
+
+# include "ft_printf.h"
+# include "libft.h"
+# include <fcntl.h>
+# include <stdio.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <unistd.h>
+
+typedef struct s_command
+{
+	int		pipe_heredoc[2];
+	int		fd[2];
+	char	**path;
+	char	**env;
+	bool	here_doc;
+	int		len;
+	char	*infile;
+	char	*outfile;
+}			t_command;
+
+char		**search_path(char **env);
+t_command	init(pid_t **pids, char **argv, char **env, int argc);
+void		children_process(t_command *command, pid_t *pids, char **argv);
+void		dup_2(int new_fd, int old_fd);
+
+#endif
